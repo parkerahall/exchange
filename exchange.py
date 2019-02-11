0e9302e43df2ff6df2cda674c0ab57056a0ebe4f
Parker Hall <parkerh@mit.edu>
2019-02-10 23:58:54 -0500

    basic init for exchange server

--- exchange.py
+++ exchange.py
@@ -1,7 +1,10 @@
+import sys
 import socket
+import threading
 
 class Exchange_Server:
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self.books = {}
+        self.book_locks = {}
